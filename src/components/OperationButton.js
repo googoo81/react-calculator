d21b8e2559a5eeefcd57331ea0a830400b3bc235
googoo81 <shw9052@naver.com>
2023-04-12 10:58:44 +0900

    계산기 완성

---
+++ src/components/OperationButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "../App";
+
+export default function OperationDigitButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
